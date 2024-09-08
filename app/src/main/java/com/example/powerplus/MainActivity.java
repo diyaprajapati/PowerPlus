9a443349e1431b4a4b84dd847e41df2efeb45a0c
diyaprajapati <diyabprajapati2005@gmail.com>
2024-09-08 13:48:40 +0530

    did some change

--- app/src/main/java/com/example/powerplus/MainActivity.java
+++ app/src/main/java/com/example/powerplus/MainActivity.java
@@ -1,37 +1,3 @@
-//package com.example.powerplus;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    private Button buttonLogout;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        buttonLogout = findViewById(R.id.buttonLogout);
-//
-//        buttonLogout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // Clear user session or any other required actions
-//
-//                // Redirect to LoginActivity
-//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//                startActivity(intent);
-//                finish(); // Close the current activity
-//            }
-//        });
-//    }
-//}
-
 package com.example.powerplus;
 
 import android.os.Bundle;
